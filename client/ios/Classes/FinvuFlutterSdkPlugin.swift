aa0f154e2666fc3246ddc20c407872a94eb3176c
Pranad Waghmare <pranadw@cookiejar.co.in>
2025-10-29 00:05:44 +0530

    fix : ios native code.

--- client/ios/Classes/FinvuFlutterSdkPlugin.swift
+++ client/ios/Classes/FinvuFlutterSdkPlugin.swift
@@ -11,7 +11,7 @@ public class FinvuFlutterSdkPlugin: NSObject, FlutterPlugin, NativeFinvuManager 
             if (error != nil) {
                 completion(.failure(FlutterError(code: "\(error!.code)", message: error?.localizedDescription, details: nil)))
             } else {
-                completion(.success(NativeLoginOtpReference(reference: loginReference!.reference,snaToken: loginReference!.snaToken, authType: loginReference!.authType)))
+                completion(.success(NativeLoginOtpReference(reference: loginReference!.reference, snaToken: loginReference!.snaToken, authType: loginReference!.authType)))
             }
         }
     }
@@ -24,7 +24,7 @@ public class FinvuFlutterSdkPlugin: NSObject, FlutterPlugin, NativeFinvuManager 
         formatter.formatOptions =  [.withInternetDateTime, .withFractionalSeconds]
     }
     
-    private func convertToNativeEnvironment(environment: NativeFinvuEnvironment) -> FinvuEnvironment {
+    private func convertToNativeEnvironment(environment: FinvuEnvironment) -> FinvuSDK.FinvuEnvironment {
         switch environment {
         case .development:
             return .uat
